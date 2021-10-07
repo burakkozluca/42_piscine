b93320f12f65fb910f0b7d14884df9c77310d283
burakkozluca <83135082+burakkozluca@users.noreply.github.com>
2021-10-07 21:17:22 +0300

    Add files via upload

---
+++ C_01_check/C_01_check/ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int	x;
+	int	y;
+
+	if (*b != 0)
+	{
+		x = *a / *b;
+		y = *a % *b;
+		*a = x;
+		*b = y;
+	}
+}
---
+++ C_01_check/C_01_check/ex05/ft_putstr.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+void	ft_putchar(char a)
+{
+	write(1, &a, 1);
+}
+
+void	ft_putstr(char *str)
+{	
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(&str[i]);
+		i++;
+	}
+	return (i);
+}
