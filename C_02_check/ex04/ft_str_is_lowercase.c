fd9d123f0d9968df3c1cc1ab09221d7406fbc4c8
burakkozluca <83135082+burakkozluca@users.noreply.github.com>
2021-10-11 20:00:10 +0300

    Add files via upload

--- C_02_check/ex04/ft_str_is_lowercase.c
+++ C_02_check/ex04/ft_str_is_lowercase.c
@@ -1,2 +1,15 @@
 #include <unistd.h>
 
+int     ft_str_is_lowercase(char *str)
+{
+    int i;
+
+    i = 0;
+    while(str[i] != '\0')
+    {
+        if(str[i] >= 'a' && str[i] <= 'z')
+            return(1);
+        i++;
+    }
+    return(0);
+}
