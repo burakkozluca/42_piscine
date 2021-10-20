f45ea06c6f299bebabb925694143dd4195d16773
burakkozluca <83135082+burakkozluca@users.noreply.github.com>
2021-10-20 11:47:50 +0300

    Add files via upload

--- C_07_check/ex01/ft_range.c
+++ C_07_check/ex01/ft_range.c
@@ -3,16 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ft_range.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bkozluca <bkozluca@student.42.fr>          +#+  +:+       +#+        */
+/*   By: burakkozluca <burakkozluca@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 17:31:09 by bkozluca          #+#    #+#             */
-/*   Updated: 2021/10/19 19:38:14 by bkozluca         ###   ########.fr       */
+/*   Updated: 2021/10/20 11:47:07 by burakkozluc      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int *ft_range(int min, int max)
+int		*ft_range(int min, int max)
 {
-    
+	int		i;
+	int		*ptr;
+	int		temp;
+
+	temp = min;
+	if (min >= max)
+		return (0);
+	ptr = (int *)malloc(sizeof(int) * (max - min));
+	if (!ptr)
+		return (0);
+	i = 0;
+	while (i < (max - min))
+	{
+		ptr[i++] = temp;
+		temp++;
+	}
+	return (ptr);
 }
--- C_07_check/ex02/ft_ultimate_range.c
+++ C_07_check/ex02/ft_ultimate_range.c
@@ -3,16 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ultimate_range.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bkozluca <bkozluca@student.42.fr>          +#+  +:+       +#+        */
+/*   By: burakkozluca <burakkozluca@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 17:33:26 by bkozluca          #+#    #+#             */
-/*   Updated: 2021/10/19 19:38:15 by bkozluca         ###   ########.fr       */
+/*   Updated: 2021/10/20 11:47:08 by burakkozluc      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int ft_ultimate_range(int **range, int min, int max)
+int		ft_ultimate_range(int **range, int min, int max)
 {
-    
+	int *ptr;
+	int i;
+
+	if (min >= max)
+	{
+		*range = 0;
+		return (0);
+	}
+	ptr = (int *)malloc(sizeof(**range) * (max - min));
+	if (!ptr)
+		return (0);
+	i = 0;
+	while (min < max)
+	{
+		ptr[i++] = min;
+		min++;
+	}
+	*range = ptr;
+	return (i);
 }
